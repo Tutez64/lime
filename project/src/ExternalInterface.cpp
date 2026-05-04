2474d7514f42e91e462b785a81562388e307f303
MasterAlexS <alexandruserban150@yahoo.ro>
2026-05-04 19:03:43 +0200

    Native MP3 decoding support via minimp3

--- project/src/ExternalInterface.cpp
+++ project/src/ExternalInterface.cpp
@@ -18,6 +18,7 @@
 #include <graphics/RenderEvent.h>
 #include <media/containers/OGG.h>
 #include <media/containers/WAV.h>
+#include <media/containers/MP3.h>
 #include <media/AudioBuffer.h>
 #include <system/CFFIPointer.h>
 #include <system/Clipboard.h>
@@ -401,6 +402,13 @@ namespace lime {
 		}
 		#endif
 
+		#ifdef LIME_MP3
+        if (MP3::Decode (&resource, &audioBuffer)) {
+
+            return audioBuffer.Value (buffer);
+        }
+        #endif
+
 		return alloc_null ();
 
 	}
@@ -423,6 +431,13 @@ namespace lime {
 
 		}
 		#endif
+
+		#ifdef LIME_MP3
+        if (MP3::Decode (&resource, buffer)) {
+
+            return buffer;
+        }
+        #endif
 
 		return 0;
 
@@ -451,6 +466,13 @@ namespace lime {
 		}
 		#endif
 
+		#ifdef LIME_MP3
+        if (MP3::Decode (&resource, &audioBuffer)) {
+
+            return audioBuffer.Value (buffer);
+        }
+        #endif
+
 		return alloc_null ();
 
 	}
@@ -473,6 +495,13 @@ namespace lime {
 
 		}
 		#endif
+
+		#ifdef LIME_MP3
+        if (MP3::Decode (&resource, buffer)) {
+
+            return buffer;
+        }
+        #endif
 
 		return 0;
 
