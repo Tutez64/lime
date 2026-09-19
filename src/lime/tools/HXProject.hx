d8c4045a3e97d84b54b938753f640aec692d4404
Joseph Cloutier <player3.14@gmail.com>
2026-09-19 16:54:48 -0400

    Get correct class path for haxelibs with overriden paths.
    Co-authored-by: Tutez64 <enzopersillet@gmail.com>

--- src/lime/tools/HXProject.hx
+++ src/lime/tools/HXProject.hx
@@ -1365,11 +1365,23 @@ class HXProject extends Script
 				}
 				catch (e:Dynamic) {}
 
-				// next is the library path on a separate line
+				// next it prints the class path
+				try
+				{
+					if (FileSystem.exists(jsonPath))
+					{
+						var json = Json.parse(File.getContent(jsonPath));
+						if (json != null && json.classPath != null && json.classPath != "")
+						{
+							path = Path.addTrailingSlash(Path.combine(path, json.classPath));
+						}
+					}
+				}
+				catch (e:Dynamic) {}
 				args.push(path);
 
-				// `haxelib path` would add a final define, but the code below
-				// filters it out, so it isn't worth including
+				// finally it would print a haxedef for the library, but we
+				// filter it out
 				// args.push("-D " + haxelibName + "=" + haxelibVersion);
 			}
 			else
